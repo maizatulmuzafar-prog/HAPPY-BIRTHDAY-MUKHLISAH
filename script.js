fef71a998b2b65c9df2f7746f44c3edb8d545137
maizatulmuzafar-prog <maizatulmuzafar@gmail.com>
2026-07-11 02:46:41 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+function openGift() {
+    document.getElementById("lock-screen").style.display = "none";
+    document.getElementById("main-content").style.display = "block";
+}
+</script>
